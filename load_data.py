c5ca6c575485ee5e0476d102f2796ad6abe5b5b4
Hilary <caihuan_academy@163.com>
2020-08-24 13:25:16 +0800

    successfully load data, and save it to file

--- load_data.py
+++ load_data.py
@@ -2,6 +2,18 @@ import numpy as np
 import pandas as pd 
 import os
 from keras.utils import to_categorical
+root_dir = 'C:\\Users\\Impos\\Desktop\\MDD\\BCI_MDD\\'
+
+########################################################
+# check one path is exist, if not, make dir
+########################################################
+def check_path(dir):
+    if not os.path.exists(dir):
+        try:
+            os.makedirs(dir)
+        except:
+            print('make dir error')
+            return
 
 ########################################################
 # load training set (1240 samples, 128*500*1)
@@ -16,7 +28,7 @@ def load_tr_data():
     tr_data = []
     tr_label = []
     for cnt, sub_name in enumerate(os.listdir(tr_data_dir)):
-        print('loading {}.csv , {}/{} sub'.format(sub_name,cnt+1,len(os.listdir(tr_data_dir))),end='  ')
+        print('loading \033[0;32;m{}\033[0m.csv , \033[0;32;m{}\033[0m/\033[0;32;m{}\033[0m sub'.format(sub_name,cnt+1,len(os.listdir(tr_data_dir))),end='  ')
         tr_sub_path = os.path.join(tr_data_dir, sub_name)
         if sub_name[0]=='D':                                                    
             sub_label = np.ones((len(os.listdir(tr_sub_path)),))
@@ -33,24 +45,25 @@ def load_tr_data():
         else:
             tr_data = np.concatenate((tr_data,sub_data),axis=0)
             tr_label = np.concatenate((tr_label,sub_label))
-        print("data.shape:{}, label.shape:{}".format(tr_data.shape, tr_label.shape))
+        print("  data.shape:{}, label.shape:{}".format(tr_data.shape, tr_label.shape))
     print("Reshape the data and label......")
     tr_data = np.expand_dims(tr_data,axis=3)
     tr_label = tr_label.reshape((tr_label.shape[0], 1))
-    train_y = to_categorical(tr_label)
+    tr_label_binary = to_categorical(tr_label)
 
     print("Randomlize the index......")
     rand_inx = np.random.permutation(range(tr_data.shape[0]))
     tr_data = tr_data[rand_inx]
     tr_label = tr_label[rand_inx]
-    print("successfully load training data {} training label {}".format(tr_data.shape, tr_label.shape))
-    return tr_data, tr_label
+    tr_label_binary = tr_label_binary[rand_inx]
+    print("successfully load tr_data \033[0;32;m{}\033[0m tr_label \033[0;32;m{}\033[0m tr_label_binary \033[0;32;m{}\033[0m".format(tr_data.shape, tr_label.shape, tr_label_binary.shape))
+    return tr_data, tr_label, tr_label_binary
 
 ########################################################
 # load validation set (440 samples, 128*500)
 # Input: None
 # Output: an array of (sampels, channels, sample_rates, 1)
-########################################################'''erect model'''
+########################################################
 
 def load_val_data():
     tr_root_dir = '..\\Validation_Set'
@@ -68,5 +81,22 @@ def load_val_data():
         if tr_data.shape[0]%50==0:
             print("val_data.shape:{}".format(tr_data.shape))
     tr_data = np.expand_dims(tr_data,axis=3)
-    print("Finnaly, val_data.shape:{}".format(tr_data.shape))
+    print("Finnaly, val_data.shape:\033[0;32;m{}\033[0m".format(tr_data.shape))
     return val_file_list,tr_data
+
+
+########################################################
+# load and save data to file
+########################################################
+tr_data, tr_label, tr_label_binary = load_tr_data()
+val_file_list,val_data = load_val_data()
+
+processed_data_dir = root_dir+'processed_data\\'
+check_path(processed_data_dir)
+
+np.save(processed_data_dir+'tr_data.npy',tr_data)
+np.save(processed_data_dir+'tr_label.npy',tr_label)
+np.save(processed_data_dir+'tr_label_binary.npy',tr_label_binary)
+np.save(processed_data_dir+'val_file_list.npy',val_file_list)
+np.save(processed_data_dir+'val_data.npy',val_data)
+
--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from load_data import load_tr_data,load_val_data
-
 
 
 '''train'''
-tr_data_dir, tr_label = load_tr_data()
-val_file_list,tr_data = load_val_data()
+
 
 
 '''evaluate'''
